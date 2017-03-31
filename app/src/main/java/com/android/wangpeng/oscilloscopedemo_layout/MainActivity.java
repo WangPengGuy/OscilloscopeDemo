6fd14da2cb3d31628737f42d918cd28f8bf00ef2
Wang Peng <wangpengguy@gmail.com>
2017-03-31 20:52:09 +0800

    添加了X、Y轴分辨率选择，Channel1、Channel2水平基线移动、时间前后标基线移动。

--- app/src/main/java/com/android/wangpeng/oscilloscopedemo_layout/MainActivity.java
+++ app/src/main/java/com/android/wangpeng/oscilloscopedemo_layout/MainActivity.java
@@ -13,16 +13,24 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener,View.OnClickListener {
 
     ImageView img_time_line_left, img_time_line_right; //时间差前标，时间差后标
     ImageView img_CH1_horizontal_line, img_CH2_horizontal_line; //通道1波形水平基线，通道2波形水平基线
+    Button btn_x_axis_add, btn_x_axis_reduce;
+    Button btn_y_axis_add, btn_y_axis_reduce;
+    TextView text_x_axis_resolution, text_y_axis_resolution;
     SurfaceView surfaceView;
+
+    XAxisResolutionChanged xAxisResolutionChanged;
+    YAxisResolutionChanged yAxisResolutionChanged;
 
     int[] location_img_time_line_left; //记录 时间差前标 在父容器中 的坐标
     int[] location_img_time_line_right; //记录 时间差后标 在父容器中 的坐标
@@ -39,26 +47,42 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     final static int WAVE = 6;
     static int WHICH_VIEW_MOVE = NONE;
     static int WHICH_VIEW_GROUP_MOVE = NONE;
+    private int clickTimes; //点击次数
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
+        //控件
         surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-
-        location_img_time_line_left = new int[2];
-        location_img_time_line_right = new int[2];
-        location_img_CH1_horizontal_line = new int[2];
-        location_img_CH2_horizontal_line = new int[2];
-
         img_time_line_left = (ImageView) findViewById(R.id.img_time_left);
         img_time_line_right = (ImageView) findViewById(R.id.img_time_right);
         img_CH1_horizontal_line = (ImageView) findViewById(R.id.img_wave_CH1_horizontal_line);
         img_CH2_horizontal_line = (ImageView) findViewById(R.id.img_wave_CH2_horizontal_line);
+        btn_x_axis_add = (Button) findViewById(R.id.btn_x_axis_add);
+        btn_x_axis_reduce = (Button) findViewById(R.id.btn_x_axis_reduce);
+        btn_y_axis_add = (Button) findViewById(R.id.btn_y_axis_add);
+        btn_y_axis_reduce = (Button) findViewById(R.id.btn_y_axis_reduce);
+        text_x_axis_resolution = (TextView) findViewById(R.id.text_X_axis_resolution);
+        text_y_axis_resolution = (TextView) findViewById(R.id.text_Y_axis_resolution);
+        //新建的类
+        xAxisResolutionChanged = new XAxisResolutionChanged(btn_x_axis_add, btn_x_axis_reduce, text_x_axis_resolution);
+        yAxisResolutionChanged = new YAxisResolutionChanged(btn_y_axis_add, btn_y_axis_reduce, text_y_axis_resolution);
+        //类的方法
 
+        //需要的数据
+        location_img_time_line_left = new int[2];
+        location_img_time_line_right = new int[2];
+        location_img_CH1_horizontal_line = new int[2];
+        location_img_CH2_horizontal_line = new int[2];
+        //绑定的事件
         surfaceView.setOnTouchListener(this);
+        btn_x_axis_add.setOnClickListener(this);
+        btn_x_axis_reduce.setOnClickListener(this);
+        btn_y_axis_add.setOnClickListener(this);
+        btn_y_axis_reduce.setOnClickListener(this);
 
     }
 
@@ -99,7 +123,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     /**
      * FUNCTION: 判断准备让哪一个View移动
      * @param touch_x   //触摸点X坐标
-     * @param touvh_y   //触摸点Y坐标
+     * @param touch_y   //触摸点Y坐标
      * @param img_time_line_left   //时间差左标线
      * @param img_time_line_right  //时间差右标线
      * @param img_CH1_horizontal_line  //通道1波形基线
@@ -109,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
      * @param location_img_CH1_horizontal_line  //通道1波形基线 坐标 二维数列
      * @param location_img_CH2_horizontal_line  //通道2波形基线 坐标 二维数列
      */
-    public void judgeViewLocation(int touch_x, int touvh_y,
+    public void judgeViewLocation(int touch_x, int touch_y,
                                   View img_time_line_left, View img_time_line_right, View img_CH1_horizontal_line, View img_CH2_horizontal_line,
                                   int[] location_img_time_line_left, int[] location_img_time_line_right,
                                   int[] location_img_CH1_horizontal_line, int[] location_img_CH2_horizontal_line) {
@@ -119,8 +143,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         img_CH2_horizontal_line.getLocationInWindow(location_img_CH2_horizontal_line);
         int dx_time_left = Math.abs(touch_x - location_img_time_line_left[0]);
         int dx_time_right = Math.abs(touch_x - location_img_time_line_right[0]);
-        int dy_ch1_horizontal = Math.abs(touvh_y - location_img_CH1_horizontal_line[1]);
-        int dy_ch2_horizontal = Math.abs(touvh_y - location_img_CH2_horizontal_line[1]);
+        int dy_ch1_horizontal = Math.abs(touch_y - location_img_CH1_horizontal_line[1]);
+        int dy_ch2_horizontal = Math.abs(touch_y - location_img_CH2_horizontal_line[1]);
         if(dx_time_left <= 20 || dx_time_right <= 20){
             WHICH_VIEW_GROUP_MOVE = TIME;
             if(dx_time_left <= 20){
@@ -185,5 +209,23 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_x_axis_add:
+                xAxisResolutionChanged.onClick(btn_x_axis_add);
+                break;
+            case R.id.btn_x_axis_reduce:
+                xAxisResolutionChanged.onClick(btn_x_axis_reduce);
+                break;
+            case R.id.btn_y_axis_add:
+                yAxisResolutionChanged.onClick(btn_y_axis_add);
+                break;
+            case R.id.btn_y_axis_reduce:
+                yAxisResolutionChanged.onClick(btn_y_axis_reduce);
+                break;
+        }
     }
 }
